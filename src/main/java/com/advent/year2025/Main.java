d20ad3eac2c598f46e17c0044284edf2be24c48c
Amari <amaristhorne@outlook.com>
2025-12-08 00:26:55 -0500

    Commented calls for day one made calls for day two

--- src/main/java/com/advent/year2025/Main.java
+++ src/main/java/com/advent/year2025/Main.java
@@ -1,15 +1,20 @@
 package com.advent.year2025;
 
-import com.advent.year2025.day_one.Day_one;
+import com.advent.year2025.Day_Two.Day_two;
 
 import java.io.IOException;
 
 public class Main {
     public static void main(String[] args) throws IOException {
 
-        Day_one.part_a(false);
-        Day_one.part_b(false);
+        //Day_one.part_a(false);
+        //Day_one.part_b(false);
+        Day_two.calc(false, false);
+        Day_two.calc(false, true);
 
-    }
+
+}
+
+
 }
 
