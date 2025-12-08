f28392a4b74936f9d561774cd4ad138d53ea0592
Amari <amaristhorne@outlook.com>
2025-12-08 00:28:21 -0500

    Added print statement so it prints which file its reading. To avoid further incidents where im telling it to use day 1's inputs for other days

--- src/main/java/com/advent/year2025/Utility.java
+++ src/main/java/com/advent/year2025/Utility.java
@@ -7,19 +7,20 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class Utility {
-    public static List<String> readInput(boolean test,int day,boolean partb) throws IOException {
-        String testdir="src/main/resources/";
-        if(test){
-            testdir+= (partb)? "test_b_":"test_a_";
-        }
-        else {
-            testdir+= (partb)? "input_b_":"input_a_";
+    public static List<String> readInput(boolean test, int day, boolean partb) throws IOException {
+        String testdir = "src/main/resources/";
+        if (test) {
+            testdir += (partb) ? "test_b_" : "test_a_";
+        } else {
+            testdir += (partb) ? "input_b_" : "input_a_";
 
         }
-        BufferedReader br=new BufferedReader( new FileReader(testdir+"day"+day+".txt"));
-        List<String> list=new ArrayList<>();
+        testdir = testdir + "day" + day + ".txt";
+        System.out.println("Reading input files from " + testdir);
+        BufferedReader br = new BufferedReader(new FileReader(testdir));
+        List<String> list = new ArrayList<>();
         String line;
-        while((line=br.readLine())!= null){
+        while ((line = br.readLine()) != null) {
             list.add(line);
 
         }
